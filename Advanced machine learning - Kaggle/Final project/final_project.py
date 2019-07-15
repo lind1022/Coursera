159765737f46ad4d08a3c7369b0aedeb92d252db
lind1022 <lukeintotherain@msn.com>
2019-07-15 22:24:37 +1200

    2nd commit

---
+++ Advanced machine learning - Kaggle/Final project/final_project.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import seaborn; seaborn.set()
+import os
+import pandas as pd
+import numpy as np
+import PyQt5
+%matplotlib qt
+
+# os.chdir("C:/Lin/Data science/Py training/Python DS handbook")
+
+DATA_FOLDER = 'C:/Users/lind/Coursera/Advanced machine learning - Kaggle/Final project'
+
+transactions    = pd.read_csv(os.path.join(DATA_FOLDER, 'sales_train.csv.gz'))
+items           = pd.read_csv(os.path.join(DATA_FOLDER, 'items.csv'))
+item_categories = pd.read_csv(os.path.join(DATA_FOLDER, 'item_categories.csv'))
+shops           = pd.read_csv(os.path.join(DATA_FOLDER, 'shops.csv'))
+
+# Creating year, month, day variable from date
+transactions['date'] = pd.to_datetime(transactions['date'], format='%d.%m.%Y')
+
+transactions['year'] = pd.DatetimeIndex(transactions['date']).year
+transactions['month'] = pd.DatetimeIndex(transactions['date']).month
+transactions['day'] = pd.DatetimeIndex(transactions['date']).day
