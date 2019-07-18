ed24ea13ea65045b6b58842aa7c1bf8b63a96375
lind1022 <lukeintotherain@msn.com>
2019-07-18 17:16:15 +1200

    more EDA

--- Advanced machine learning - Kaggle/Final project/final_project.py
+++ Advanced machine learning - Kaggle/Final project/final_project.py
@@ -6,6 +6,11 @@ import numpy as np
 import PyQt5
 import statsmodels.api as sm
 from pylab import rcParams
+import itertools
+from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from pandas.plotting import autocorrelation_plot
+from statsmodels.tsa.stattools import adfuller, acf, pacf,arma_order_select_ic
 %matplotlib qt
 
 # os.chdir("C:/Lin/Data science/Py training/Python DS handbook")
@@ -37,27 +42,133 @@ transactions = transactions.drop(columns = 'item_name')
 # Sort by date
 transactions = transactions.sort_values('date')
 
-# check missing value
+# check missing value and unique values for each column
 transactions.isnull().sum()
+transactions.nunique()
 
 # Look at the overall sales first
-overall = transactions.groupby('date')['sales'].sum().reset_index()
+overall_ts = transactions.groupby('date_block_num')['item_cnt_day'].sum()
 
-overall = overall.set_index('date')
-overall.index
+# overall_ts = overall_ts.set_index('date_block_num')
+# overall_ts.index
 
 # Look at the overall sales time series
-overall.plot(figsize=(15, 6))
+overall_ts.plot(figsize=(15, 6))
+plt.title('Total Sales of the company')
+plt.xlabel('Time')
+plt.ylabel('Sales')
 plt.show()
 
 # Time-series docomposition to decompose into trend, seasonality and noise
-rcParams['figure.figsize'] = 18, 8
-decomposition = sm.tsa.seasonal_decompose(overall, model='additive')
+decomposition = sm.tsa.seasonal_decompose(overall_ts, freq = 12, model='multiplicative')
 fig = decomposition.plot()
 plt.show()
 
+# Stationarity tests
+def test_stationarity(timeseries):
+
+    #Perform Dickey-Fuller test:
+    print('Results of Dickey-Fuller Test:')
+    dftest = adfuller(timeseries, autolag='AIC')
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+    for key,value in dftest[4].items():
+        dfoutput['Critical Value (%s)'%key] = value
+    print (dfoutput)
+
+test_stationarity(overall_ts)
+
+
+# De-trending and de-seasonalization
+def difference(dataset, interval=1):
+    diff = list()
+    for i in range(interval, len(dataset)):
+        value = dataset[i] - dataset[i - interval]
+        diff.append(value)
+    return pd.Series(diff)
+
+
+overall_ts.astype('float')
+plt.figure(figsize=(20,16))
+plt.subplot(311)
+plt.title('Original')
+plt.xlabel('Time')
+plt.ylabel('Sales')
+plt.plot(overall_ts)
+
+plt.subplot(312)
+plt.title('After De-trend')
+plt.xlabel('Time')
+plt.ylabel('Sales')
+new_ts=difference(overall_ts)
+plt.plot(new_ts)
+# plt.plot()
+
+plt.subplot(313)
+plt.title('After De-seasonalization')
+plt.xlabel('Time')
+plt.ylabel('Sales')
+new_ts=difference(overall_ts,12)       # assuming the seasonality is 12 months long
+plt.plot(new_ts)
+# plt.plot()
+
+# Re-testing the stationarity after de-seasonalization
+test_stationarity(new_ts)
+
+
+############
+# Forecast
+############
+# MA - Next value in the series is a function of the average of the previous n number of values
+# AR - The errors(difference in mean) of the next value is a function of the errors in the previous n number of values
+# ARMA - a mixture of both.
+
+# Need to determine if our series is AR, MA or bot
+def tsplot(y, lags=None, figsize=(10, 8), style='bmh',title=''):
+    if not isinstance(y, pd.Series):
+        y = pd.Series(y)
+    with plt.style.context(style):
+        fig = plt.figure(figsize=figsize)
+        #mpl.rcParams['font.family'] = 'Ubuntu Mono'
+        layout = (3, 2)
+        ts_ax = plt.subplot2grid(layout, (0, 0), colspan=2)
+        acf_ax = plt.subplot2grid(layout, (1, 0))
+        pacf_ax = plt.subplot2grid(layout, (1, 1))
+        qq_ax = plt.subplot2grid(layout, (2, 0))
+        pp_ax = plt.subplot2grid(layout, (2, 1))
+
+        y.plot(ax=ts_ax)
+        ts_ax.set_title(title)
+        smt.graphics.plot_acf(y, lags=lags, ax=acf_ax, alpha=0.5)
+        smt.graphics.plot_pacf(y, lags=lags, ax=pacf_ax, alpha=0.5)
+        sm.qqplot(y, line='s', ax=qq_ax)
+        qq_ax.set_title('QQ Plot')
+        scs.probplot(y, sparams=(y.mean(), y.std()), plot=pp_ax)
+
+        plt.tight_layout()
+    return
+
+# Simulate a AR(1) process
+np.random.seed(1)
+n_samples = int(1000)
+a = 0.6
+x = w = np.random.normal(size=n_samples)
+
+for t in range(n_samples):
+    x[t] = a*x[t-1] + w[t]
+limit=12
+_ = tsplot(x, lags=limit,title="AR(1)process")
+
+
+
+
+
+
+
+
+
+
 # ARIMA baseline model
-p = d = q = range(2, 4)
+p = d = q = range(1, 2)
 pdq = list(itertools.product(p, d, q))
 seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
 
@@ -71,7 +182,7 @@ aic = []
 for param in pdq:
     for param_seasonal in seasonal_pdq:
         try:
-            mod = sm.tsa.statespace.SARIMAX(overall,
+            mod = sm.tsa.statespace.SARIMAX(overall_ts,
                                             order=param,
                                             seasonal_order=param_seasonal,
                                             enforce_stationarity=False,
@@ -81,8 +192,6 @@ for param in pdq:
             aic.append('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
         except:
             continue
-
-
 
 
 
