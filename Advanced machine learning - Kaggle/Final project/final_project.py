7b67f2808940700b56bd2064fa3964d4afdbb133
lind1022 <lukeintotherain@msn.com>
2019-08-28 18:16:24 +1200

    comments on final assignment

--- Advanced machine learning - Kaggle/Final project/final_project.py
+++ Advanced machine learning - Kaggle/Final project/final_project.py
@@ -15,6 +15,15 @@ from statsmodels.tsa.stattools import adfuller, acf, pacf,arma_order_select_ic
 
 # os.chdir("C:/Lin/Data science/Py training/Python DS handbook")
 
+"""
+A good exercise is to reproduce previous_value_benchmark. As the name suggest - in this benchmark for the each shop/item pair our predictions are just monthly sales from the previous month, i.e. October 2015.
+
+The most important step at reproducing this score is correctly aggregating daily data and constructing monthly sales data frame. You need to get lagged values, fill NaNs with zeros and clip the values into [0,20] range. If you do it correctly, you'll get precisely 1.16777 on the public leaderboard.
+
+Generating features like this is a necessary basis for more complex models. Also, if you decide to fit some model, don't forget to clip the target into [0,20] range, it makes a big difference.
+"""
+
+
 DATA_FOLDER = 'C:/Users/lind/Coursera/Advanced machine learning - Kaggle/Final project'
 
 # DATA_FOLDER = 'C:/Lin/Data science/Github repo/Coursera/Advanced machine learning - Kaggle/Final project'
