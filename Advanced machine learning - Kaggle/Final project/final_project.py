1c0899e479aa5b641522233ea85c5c3b840de010
lind1022 <lukeintotherain@msn.com>
2019-09-27 21:51:38 +1200

    start another one

--- Advanced machine learning - Kaggle/Final project/final_project.py
+++ Advanced machine learning - Kaggle/Final project/final_project.py
@@ -17,339 +17,70 @@ from pandas.plotting import autocorrelation_plot
 from statsmodels.tsa.stattools import adfuller, acf, pacf,arma_order_select_ic
 %matplotlib qt
 
-# os.chdir("C:/Lin/Data science/Py training/Python DS handbook")
 
 """
-A good exercise is to reproduce previous_value_benchmark. As the name suggest - in this benchmark for the each shop/item pair our
-predictions are just monthly sales from the previous month, i.e. October 2015.
+Some Tips
 
-The most important step at reproducing this score is correctly aggregating daily data and constructing monthly sales data frame.
-You need to get lagged values, fill NaNs with zeros and clip the values into [0,20] range. If you do it correctly, you'll get precisely 1.16777 on the public leaderboard.
+A good exercise is to reproduce previous_value_benchmark. As the name suggest -
+ in this benchmark for the each shop/item pair our predictions are just monthly
+ sales from the previous month, i.e. October 2015.
 
-Generating features like this is a necessary basis for more complex models. Also, if you decide to fit some model, don't forget to clip the target into [0,20] range, it makes a big difference.
+The most important step at reproducing this score is correctly aggregating daily
+data and constructing monthly sales data frame.
+You need to get lagged values, fill NaNs with zeros and clip the values into
+[0,20] range. If you do it correctly, you'll get precisely 1.16777 on the public
+leaderboard.
+
+Generating features like this is a necessary basis for more complex models.
+Also, if you decide to fit some model, don't forget to clip the target into
+[0,20] range, it makes a big difference.
+
+You can get a rather good score after creating some lag-based features like in
+ advice from previous week and feeding them into gradient boosted trees model.
+
+Apart from item/shop pair lags you can try adding lagged values of total shop or
+ total item sales (which are essentially mean-encodings). All of that is going
+ to add some new information.
 """
 
 
-DATA_FOLDER = 'C:/Users/lind/Coursera/Advanced machine learning - Kaggle/Final project'
+# DATA_FOLDER = 'C:/Users/lind/Coursera/Advanced machine learning - Kaggle/Final project'
 
-# DATA_FOLDER = 'C:/Lin/Data science/Github repo/Coursera/Advanced machine learning - Kaggle/Final project'
+DATA_FOLDER = 'C:/Lin/Data science/Github repo/Coursera/Advanced machine learning - Kaggle/Final project'
 
-transactions    = pd.read_csv(os.path.join(DATA_FOLDER, 'sales_train.csv.gz'))
+trans           = pd.read_csv(os.path.join(DATA_FOLDER, 'sales_train.csv.gz'))
 items           = pd.read_csv(os.path.join(DATA_FOLDER, 'items.csv'))
 item_categories = pd.read_csv(os.path.join(DATA_FOLDER, 'item_categories.csv'))
 shops           = pd.read_csv(os.path.join(DATA_FOLDER, 'shops.csv'))
 
 # Creating year, month, day variable from date
-transactions['date'] = pd.to_datetime(transactions['date'], format='%d.%m.%Y')
+trans['date'] = pd.to_datetime(trans['date'], format='%d.%m.%Y')
 
-transactions['year'] = pd.DatetimeIndex(transactions['date']).year
-transactions['month'] = pd.DatetimeIndex(transactions['date']).month
-transactions['day'] = pd.DatetimeIndex(transactions['date']).day
+trans['year'] = pd.DatetimeIndex(trans['date']).year
+trans['month'] = pd.DatetimeIndex(trans['date']).month
+trans['day'] = pd.DatetimeIndex(trans['date']).day
 
 # Creating a sales column for each day
-transactions['sales'] = transactions['item_price'] * transactions['item_cnt_day']
-transactions = pd.merge(transactions, items, on='item_id')
+trans['sales'] = trans['item_price'] * trans['item_cnt_day']
+trans = pd.merge(trans, items, on='item_id')
 # The time series range
-print('Timeseries start from ' + str(transactions['date'].min()) + ', finish on ' + str(transactions['date'].max()))
+print('Timeseries start from ' + str(trans['date'].min()) + ', finish on ' + str(trans['date'].max()))
 # Drop item name column, consider it as un-useful for now
-transactions = transactions.drop(columns = 'item_name')
+trans = trans.drop(columns = 'item_name')
 
 # Sort by date
-transactions = transactions.sort_values('date')
+trans = trans.sort_values('date')
 
 # check missing value and unique values for each column
-transactions.isnull().sum()
-transactions.nunique()
-
-# Look at the overall sales first
-overall_ts = transactions.groupby('date_block_num')['item_cnt_day'].sum()
-
-# overall_ts = overall_ts.set_index('date_block_num')
-# overall_ts.index
-
-# Look at the overall sales time series
-overall_ts.plot(figsize=(15, 6))
-plt.title('Total Sales of the company')
-plt.xlabel('Time')
-plt.ylabel('Sales')
-plt.show()
-
-# Time-series docomposition to decompose into trend, seasonality and noise
-decomposition = sm.tsa.seasonal_decompose(overall_ts, freq = 12, model='multiplicative')
-fig = decomposition.plot()
-plt.show()
-
-# Stationarity tests
-def test_stationarity(timeseries):
-
-    #Perform Dickey-Fuller test:
-    print('Results of Dickey-Fuller Test:')
-    dftest = adfuller(timeseries, autolag='AIC')
-    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
-    for key,value in dftest[4].items():
-        dfoutput['Critical Value (%s)'%key] = value
-    print (dfoutput)
-
-test_stationarity(overall_ts)
+trans.isnull().sum()
+trans.nunique()
 
 
-# De-trending and de-seasonalization
-def difference(dataset, interval=1):
-    diff = list()
-    for i in range(interval, len(dataset)):
-        value = dataset[i] - dataset[i - interval]
-        diff.append(value)
-    return pd.Series(diff)
-
-
-overall_ts.astype('float')
-plt.figure(figsize=(20,16))
-plt.subplot(311)
-plt.title('Original')
-plt.xlabel('Time')
-plt.ylabel('Sales')
-plt.plot(overall_ts)
-
-plt.subplot(312)
-plt.title('After De-trend')
-plt.xlabel('Time')
-plt.ylabel('Sales')
-new_ts=difference(overall_ts)
-plt.plot(new_ts)
-# plt.plot()
-
-plt.subplot(313)
-plt.title('After De-seasonalization')
-plt.xlabel('Time')
-plt.ylabel('Sales')
-new_ts=difference(overall_ts,12)       # assuming the seasonality is 12 months long
-plt.plot(new_ts)
-# plt.plot()
-
-# Re-testing the stationarity after de-seasonalization
-test_stationarity(new_ts)
-
-
-############
-# Forecast
-############
-# MA - Next value in the series is a function of the average of the previous n number of values
-# AR - The errors(difference in mean) of the next value is a function of the errors in the previous n number of values
-# ARMA - a mixture of both.
-
-# Need to determine if our series is AR, MA or bot
-def tsplot(y, lags=None, figsize=(10, 8), style='bmh',title=''):
-    if not isinstance(y, pd.Series):
-        y = pd.Series(y)
-    with plt.style.context(style):
-        fig = plt.figure(figsize=figsize)
-        #mpl.rcParams['font.family'] = 'Ubuntu Mono'
-        layout = (3, 2)
-        ts_ax = plt.subplot2grid(layout, (0, 0), colspan=2)
-        acf_ax = plt.subplot2grid(layout, (1, 0))
-        pacf_ax = plt.subplot2grid(layout, (1, 1))
-        qq_ax = plt.subplot2grid(layout, (2, 0))
-        pp_ax = plt.subplot2grid(layout, (2, 1))
-
-        y.plot(ax=ts_ax)
-        ts_ax.set_title(title)
-        smt.graphics.plot_acf(y, lags=lags, ax=acf_ax, alpha=0.5)
-        smt.graphics.plot_pacf(y, lags=lags, ax=pacf_ax, alpha=0.5)
-        sm.qqplot(y, line='s', ax=qq_ax)
-        qq_ax.set_title('QQ Plot')
-        scs.probplot(y, sparams=(y.mean(), y.std()), plot=pp_ax)
-
-        plt.tight_layout()
-    return
-
-# Simulate a AR(1) process with Alpha = 0.6
-np.random.seed(1)
-n_samples = int(1000)
-a = 0.6
-x = w = np.random.normal(size=n_samples)
-
-for t in range(n_samples):
-    x[t] = a*x[t-1] + w[t]
-limit=12
-_ = tsplot(x, lags=limit,title="AR(1)process")
-
-# Simulate a AR(2) process
-n = int(1000)
-alphas = np.array([.444, .333])
-betas = np.array([0.])
-
-# Python requires us to specify the zero-lag value which is 1
-# Also note that the alphas for the AR model must be negated
-# We also set the betas for the MA equal to 0 for an AR(p) model
-# For more information see the examples at statsmodels.org
-ar = np.r_[1, -alphas]
-ma = np.r_[1, betas]
-
-ar2 = smt.arma_generate_sample(ar=ar, ma=ma, nsample=n)
-_ = tsplot(ar2, lags=12,title="AR(2) process")
-
-# Simulate an MA(1) process
-n = int(1000)
-# set the AR(p) alphas equal to 0
-alphas = np.array([0.])
-betas = np.array([0.8])
-# add zero-lag and negate alphas
-ar = np.r_[1, -alphas]
-ma = np.r_[1, betas]
-ma1 = smt.arma_generate_sample(ar=ar, ma=ma, nsample=n)
-limit=12
-_ = tsplot(ma1, lags=limit,title="MA(1) process")
-
-# Simulate MA(2) process with betas 0.6, 0.4
-n = int(1000)
-alphas = np.array([0.])
-betas = np.array([0.6, 0.4])
-ar = np.r_[1, -alphas]
-ma = np.r_[1, betas]
-
-ma3 = smt.arma_generate_sample(ar=ar, ma=ma, nsample=n)
-_ = tsplot(ma3, lags=12,title="MA(2) process")
-
-
-
-# Simulate an ARMA(2, 2) model with alphas=[0.5,-0.25] and betas=[0.5,-0.3]
-max_lag = 12
-
-n = int(5000) # lots of samples to help estimates
-burn = int(n/10) # number of samples to discard before fit
-
-alphas = np.array([0.8, -0.65])
-betas = np.array([0.5, -0.7])
-ar = np.r_[1, -alphas]
-ma = np.r_[1, betas]
-
-arma22 = smt.arma_generate_sample(ar=ar, ma=ma, nsample=n, burnin=burn)
-_ = tsplot(arma22, lags=max_lag,title="ARMA(2,2) process")
-
-# pick best order by aic
-# smallest aic value wins
-best_aic = np.inf
-best_order = None
-best_mdl = None
-
-rng = range(5)
-for i in rng:
-    for j in rng:
-        try:
-            tmp_mdl = smt.ARMA(arma22, order=(i, j)).fit(method='mle', trend='nc')
-            tmp_aic = tmp_mdl.aic
-            if tmp_aic < best_aic:
-                best_aic = tmp_aic
-                best_order = (i, j)
-                best_mdl = tmp_mdl
-        except: continue
-
-
-print('aic: {:6.5f} | order: {}'.format(best_aic, best_order))
-
-
-###################
-# Facebook Prophet
-###################
-from fbprophet import Prophet
-from fbprophet.plot import plot_plotly
-import plotly.offline as py
-
-
-overall_ts.index=pd.date_range(start = '2013-01-01',end='2015-10-01', freq = 'MS')
-overall_ts=overall_ts.reset_index()
-overall_ts.head()
-
-overall_ts.columns=['ds','y']
-model = Prophet( yearly_seasonality=True) #instantiate Prophet with only yearly seasonality as our data is monthly
-model.fit(overall_ts)
-
-
-# predict for five months in the furure and MS - month start is the frequency
-future = model.make_future_dataframe(periods = 5, freq = 'MS')
-# now lets make the forecasts
-forecast = model.predict(future)
-forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
-
-model.plot(forecast)
-model.plot_components(forecast)
-
-py.init_notebook_mode()
-
-fig = plot_plotly(model, forecast)  # This returns a interactive plotly Figure
-py.plot(fig)
-
-
-
-# Middle out approach
-# Eg: Consider our problem of predicting store-item level forecasts.
-# Take the middle level(Stores) and find forecasts for the stores
-# Use bottoms up approach to find overall sales
-# Dis-integrate store sales using proportions to find the item-level sales using a top-down approach
-
-# get the unique combinations of item-store from the sales data at monthly level
-monthly_shop_sales=transactions.groupby(["date_block_num","shop_id"])["item_cnt_day"].sum()
-# get the shops to the columns
-monthly_shop_sales=monthly_shop_sales.unstack(level=1)
-
-num_of_shops = len(monthly_shop_sales.columns)
-
-monthly_shop_sales=monthly_shop_sales.fillna(0)
-
-dates=pd.date_range(start = '2013-01-01',end='2015-10-01', freq = 'MS')
-monthly_shop_sales.index=dates
-monthly_shop_sales=monthly_shop_sales.reset_index()
-monthly_shop_sales.head()
-
-# Looping through each stores to forecast
-import time
-start_time=time.time()
-
-# Calculating the base forecasts using prophet
-# From HTSprophet pachage -- https://github.com/CollinRooney12/htsprophet/blob/master/htsprophet/hts.py
-forecastsDict = {}
-for node in range(num_of_shops):
-    # take the date-column and the col to be forecasted
-    nodeToForecast = pd.concat([monthly_shop_sales.iloc[:,0], monthly_shop_sales.iloc[:, node+1]], axis = 1)
-#     print(nodeToForecast.head())  # just to check
-# rename for prophet compatability
-    nodeToForecast = nodeToForecast.rename(columns = {nodeToForecast.columns[0] : 'ds'})
-    nodeToForecast = nodeToForecast.rename(columns = {nodeToForecast.columns[1] : 'y'})
-    growth = 'linear'
-    m = Prophet(growth, yearly_seasonality=True)
-    m.fit(nodeToForecast)
-    future = m.make_future_dataframe(periods = 1, freq = 'MS')
-    forecastsDict[node] = m.predict(future)
+ts = trans.groupby(['date_block_num', 'shop_id', 'item_id'])['item_cnt_day'].sum().reset_index()
 
 
 
 
-
-# ARIMA baseline model
-p = d = q = range(1, 2)
-pdq = list(itertools.product(p, d, q))
-seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
-
-print('Examples of parameter combinations for Seasonal ARIMA...')
-print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[1]))
-print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[2]))
-print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[3]))
-print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[4]))
-
-aic = []
-for param in pdq:
-    for param_seasonal in seasonal_pdq:
-        try:
-            mod = sm.tsa.statespace.SARIMAX(overall_ts,
-                                            order=param,
-                                            seasonal_order=param_seasonal,
-                                            enforce_stationarity=False,
-                                            enforce_invertibility=False)
-            results = mod.fit()
-            print('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
-            aic.append('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
-        except:
-            continue
 
 
 
