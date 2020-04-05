bde794eae5e7ff2b3b352a72b750615a435a1387
lind1022 <lukeintotherain@msn.com>
2020-04-05 21:19:02 +1200

    re-arranging pipeline

--- Advanced machine learning - Kaggle/Final project/final_project.py
+++ Advanced machine learning - Kaggle/Final project/final_project.py
@@ -102,112 +102,12 @@ trans['day'] = pd.DatetimeIndex(trans['date']).day
 # The time series range
 print('Timeseries start from ' + str(trans['date'].min()) + ', finish on ' + str(trans['date'].max()))
 
-########################
-# Feature Engineering
-########################
-
-# Sort by date
-trans = trans.sort_values('date_block_num')
-
-# Clip sales values into the [0, 20] range
-trans['item_cnt_day'][trans['item_cnt_day'] < 0] = 0
-trans['item_cnt_day'][trans['item_cnt_day'] > 20] = 20
-
-# Drop item name column, consider it as un-useful for now
-trans = trans.drop(columns = ['date'])
-
-# Control for data leakage, only use item and store that appear in the test dataset.
-test_shop_ids = test['shop_id'].unique()
-test_item_ids = test['item_id'].unique()
-
-# Keep data that has shop and item in the test dataset
-trans = trans[trans['shop_id'].isin(test_shop_ids)]
-trans = trans[trans['item_id'].isin(test_item_ids)]
-
-
-index_cols = ['shop_id', 'item_id', 'date_block_num']
-
-# For every month we create a grid from all shops/items combinations from that month
-grid = []
-for block_num in trans['date_block_num'].unique():
-    cur_shops = trans.loc[trans['date_block_num'] == block_num, 'shop_id'].unique()
-    cur_items = trans.loc[trans['date_block_num'] == block_num, 'item_id'].unique()
-    grid.append(np.array(list(product(*[cur_shops, cur_items, [block_num]])),dtype='int32'))
-
-# Turn the grid into a dataframe
-grid = pd.DataFrame(np.vstack(grid), columns = index_cols, dtype=np.int32)
-
-# Aggregate to montly data by month, shop, item
-gb = trans.groupby(['date_block_num', 'shop_id', 'item_id']).agg(target=('item_cnt_day', 'sum')).reset_index()
-train = pd.merge(grid, gb, how='left', on=['date_block_num', 'shop_id', 'item_id']).fillna(0)
-
-# Aggregate to item-month
-gb = trans.groupby(['date_block_num', 'item_id']).agg(item_month=('item_cnt_day', 'sum')).reset_index()
-train = pd.merge(train, gb, how='left', on=['date_block_num', 'item_id']).fillna(0)
-
-# Aggregate to shop-month
-gb = trans.groupby(['date_block_num', 'shop_id']).agg(shop_month=('item_cnt_day', 'sum')).reset_index()
-train = pd.merge(train, gb, how='left', on=['date_block_num', 'shop_id']).fillna(0)
-
-train = downcast_dtypes(train)
-
-
-# trans = downcast_dtypes(trans)
-
-#############################
-# Adding lag based features
-#############################
-
-# Creating a lag term
-
-cols_to_rename = list(train.columns.difference(index_cols))
-
-shift_range = [1, 2, 3, 4, 5, 12]
-
-for month_shift in shift_range:
-    train_shift = train[index_cols + cols_to_rename].copy()
-
-    train_shift['date_block_num'] = train_shift['date_block_num'] + month_shift
-
-    foo = lambda x: '{}_lag_{}'.format(x, month_shift) if x in cols_to_rename else x
-    train_shift = train_shift.rename(columns=foo)
-    train = pd.merge(train, train_shift, on=index_cols, how='left').fillna(0)
-
-del train_shift
-
-# Don't use old data from year 2013 because they don't have a lag
-train = train[train['date_block_num'] >= 12]
-
-
-# List of all lagged features
-fit_cols = [col for col in train.columns if col[-1] in [str(item) for item in shift_range]]
-
-# We will drop these at fitting stage
-to_drop_cols = list(set(list(train.columns)) - (set(fit_cols)|set(index_cols))) + ['date_block_num']
-
-# Category for each item
-item_category_mapping = items[['item_id','item_category_id']].drop_duplicates()
-item_price_mapping = trans[['item_id', 'item_price']].drop_duplicates()
-
-
-train = pd.merge(train, item_category_mapping, how='left', on='item_id')
-train = pd.merge(train, item_price_mapping, how='left', on='item_id')
-
-# gc.collect();
-
-
-
-##########################
-# Mean encoding features #
-##########################
-
 #######
-# EDA
+# EDA #
 #######
 # Grouping data for EDA.
 # Category for each item
 item_category_mapping = items[['item_id','item_category_id']].drop_duplicates()
-
 trans = pd.merge(trans, item_category_mapping, how='left', on='item_id')
 
 # Sales by month
@@ -238,6 +138,128 @@ sns.barplot(x="shop_id", y="item_cnt_day", data=gp_shop_mean, ax=axes[0], palett
 sns.barplot(x="shop_id", y="item_cnt_day", data=gp_shop_sum, ax=axes[1], palette="rocket").set_title("Monthly sum")
 plt.show()
 
+########################
+# Feature Engineering
+########################
+
+# Sort by date
+trans = trans.sort_values('date_block_num')
+
+# Clip sales values into the [0, 20] range
+trans['item_cnt_day'][trans['item_cnt_day'] < 0] = 0
+trans['item_cnt_day'][trans['item_cnt_day'] > 20] = 20
+
+# Drop item name column, consider it as un-useful for now
+trans = trans.drop(columns = ['date'])
+
+# Control for data leakage, only use item and store that appear in the test dataset.
+test_shop_ids = test['shop_id'].unique()
+test_item_ids = test['item_id'].unique()
+
+# Keep data that has shop and item in the test dataset
+trans = trans[trans['shop_id'].isin(test_shop_ids)]
+trans = trans[trans['item_id'].isin(test_item_ids)]
+
+index_cols = ['shop_id', 'item_id', 'date_block_num']
+
+
+# For every month we create a grid from all shops/items combinations from that month
+grid = []
+for block_num in trans['date_block_num'].unique():
+    cur_shops = trans.loc[trans['date_block_num'] == block_num, 'shop_id'].unique()
+    cur_items = trans.loc[trans['date_block_num'] == block_num, 'item_id'].unique()
+    grid.append(np.array(list(product(*[cur_shops, cur_items, [block_num]])),dtype='int32'))
+
+# Turn the grid into a dataframe
+grid = pd.DataFrame(np.vstack(grid), columns = index_cols, dtype=np.int32)
+
+# Aggregate to montly data by month, shop, item
+gb = trans.groupby(['date_block_num', 'shop_id', 'item_id']).agg(target=('item_cnt_day', 'sum')).reset_index()
+train = pd.merge(grid, gb, how='left', on=['date_block_num', 'shop_id', 'item_id']).fillna(0)
+
+# Aggregate to item-month
+gb = trans.groupby(['date_block_num', 'item_id']).agg(item_month=('item_cnt_day', 'sum')).reset_index()
+train = pd.merge(train, gb, how='left', on=['date_block_num', 'item_id']).fillna(0)
+
+# Aggregate to shop-month
+gb = trans.groupby(['date_block_num', 'shop_id']).agg(shop_month=('item_cnt_day', 'sum')).reset_index()
+train = pd.merge(train, gb, how='left', on=['date_block_num', 'shop_id']).fillna(0)
+
+train = downcast_dtypes(train)
+
+
+#############################
+# Adding lag based features
+#############################
+
+# Creating a lag term
+cols_to_rename = list(train.columns.difference(index_cols))
+
+shift_range = [1, 2, 3, 4, 5, 12]
+
+for month_shift in shift_range:
+    train_shift = train[index_cols + cols_to_rename].copy()
+
+    train_shift['date_block_num'] = train_shift['date_block_num'] + month_shift
+
+    foo = lambda x: '{}_lag_{}'.format(x, month_shift) if x in cols_to_rename else x
+    train_shift = train_shift.rename(columns=foo)
+    train = pd.merge(train, train_shift, on=index_cols, how='left').fillna(0)
+
+del train_shift
+
+# Don't use old data from year 2013 because they don't have a lag
+train = train[train['date_block_num'] >= 12]
+
+# Category for each item
+item_category_mapping = items[['item_id','item_category_id']].drop_duplicates()
+item_price_mapping = trans[['item_id', 'item_price']].drop_duplicates()
+
+train = pd.merge(train, item_category_mapping, how='left', on='item_id')
+train = pd.merge(train, item_price_mapping, how='left', on='item_id')
+
+
+###############################
+# Rolling Window Calculations #
+###############################
+# Rolling Min
+f_min = lambda x: x.rolling(window=3, min_periods=1).min
+# Rolling Max
+f_max = lambda x: x.rolling(window=3, min_periods=1).max
+# Rolling Mean
+f_mean = lambda x: x.rolling(window=3, min_periods=1).mean
+# Rolling Stardard Deviation
+f_sd = lambda x: x.rolling(window=3, min_periods=1).std
+
+func_list = [f_min, f_max, f_mean, f_sd]
+func_name = ['min', 'max', 'mean', 'sd']
+
+for i in range(len(func_list)):
+    train[('item_cnt_%s' % func_name[i])] = train.sort_values('date_block_num').groupby(['shop_id', 'item_category_id', 'item_id'])['item_cnt_day'].apply(function_list[i])
+
+# List of all lagged features
+fit_cols = [col for col in train.columns if col[-1] in [str(item) for item in shift_range]]
+
+# We will drop these at fitting stage
+to_drop_cols = list(set(list(train.columns)) - (set(fit_cols)|set(index_cols))) + ['date_block_num']
+
+
+
+########################
+# Group Based Features #
+########################
+# Item history min and max prices
+gp_item_price = trans.groupby(['item_id'], as_index=False).agg({'item_price':[np.min, np.max]})
+gp_item_price.columns = ['item_id', 'min_item_price', 'max_item_price']
+
+train = pd.merge(train, gp_item_price, how='left', on='item_id')
+
+# How much the price has changed since its historical high and low
+train['price_increase'] = train['max_item_price'] - train['item_price']
+train['price_decrease'] = train['item_price'] - train['min_item_price']
+
+# gc.collect();
+
 
 
 #####################
@@ -257,6 +279,11 @@ X_test =  train.loc[dates == last_block].drop(to_drop_cols, axis=1)
 
 y_train = train.loc[dates <  last_block, 'target'].values
 y_test =  train.loc[dates == last_block, 'target'].values
+
+##########################
+# Mean encoding features #
+##########################
+# To be done after train test split
 
 
 ####################################
